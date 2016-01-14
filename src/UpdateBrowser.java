d28b496e74b691bdced0a126f5864514da1cc45c
Miles <mmenafee@deerfield.edu>
2016-01-14 14:40:38 -0500

    All of the stuff

---
+++ src/UpdateBrowser.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+public class UpdateBrowser extends JApplet{
+
+	public void start(){
+		
+	}
+	
+	public void init(){
+		
+	}
+
+	public void paint(Graphics g){
+		g.drawString("Turn my swag on",100,90);
+		g.setColor(Color.BLACK);
+	}
+	}
